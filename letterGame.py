5c8616311eb0d23b880ac72209e379ef252a51b7
Dianna <dhummel1210@gmail.com>
2016-01-12 16:05:10 -0500

    adding functions

--- letterGame.py
+++ letterGame.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 #make a list of words
 words = [
@@ -12,8 +13,24 @@ words = [
     'babies'
     ]
 
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def draw(bad_guesses, good_guesses, secret_word):
+    for letter in secret_word:
+        if letter in good_guesses:
+            print(letter, end =' ')
+        else:
+            print('_', end =' ')
+    print(' ')
+    print('Strikes: {}/7'.format(len(bad_guesses)))
+    print(' ')      
+
 while True:
-    start = input('Press enter/retrun to start, or enter Q to quit')
+    start = input('Press enter/return to start, or enter Q to quit')
     if start.lower() == 'q':
         break
     
@@ -25,14 +42,7 @@ while True:
     while len(bad_guesses) < 7 and len(good_guesses) != len(list(secret_word)):
         #draw spaces
         #draw guessed letters, spaces, and strikes
-        for letter in secret_word:
-            if letter in good_guesses:
-                print(letter, end =' ')
-            else:
-                print('_', end =' ')
-        print(' ')
-        print('Strikes: {}/7'.format(len(bad_guesses)))
-        print(' ')
+        
         
     #take guess
     
