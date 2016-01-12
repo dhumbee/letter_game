b642ca393070fffaf357d13b735d4e76ebb01c59
Dianna <dhummel1210@gmail.com>
2016-01-12 09:57:12 -0500

    created list of words

--- letterGame.py
+++ letterGame.py
@@ -1,1 +1,19 @@
-#create a letter game
+import random
+
+#make a list of words
+words = [
+    'shihtzu',
+    'poodle',
+    'cute',
+    'puppies',
+    'fluffy',
+    'love',
+    'brats',
+    'babies'
+    ]
+    
+#pick random word
+#draw spaces
+#take guess
+#draw guessed letters and strikes
+#print out win/lose
